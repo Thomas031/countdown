fe6466d5717b6117243a5eea4e6c1a9cd9e962d9
Dark angel <72506107+Thomas031@users.noreply.github.com>
2024-07-25 00:28:54 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function startCountdown(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var countdownTime = 60 * 5, // Compte à rebours de 5 minutes
+        display = document.querySelector('#countdown');
+    startCountdown(countdownTime, display);
+};
